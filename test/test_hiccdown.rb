0781c912ab8ca4b49a166471d1e12e1f58078964
Dennis C Hackethal <dennis.hackethal@gmail.com>
2021-05-04 09:55:24 -0700

    Housekeeping

--- test/test_hiccdown.rb
+++ test/test_hiccdown.rb
@@ -3,23 +3,23 @@ require 'hiccdown'
 
 class HiccdownTest < Minitest::Test
   def test_convert_tag_with_attrs_but_no_content
-    assert_equal Hiccdown::to_html([:p, {class: 'foo'}]), '<p class="foo"></p>'
+    assert_equal '<p class="foo"></p>', Hiccdown::to_html([:p, {class: 'foo'}])
   end
 
   def test_convert_tag_with_content_but_no_attrs
-    assert_equal Hiccdown::to_html([:p, 'foo']), '<p>foo</p>'
+    assert_equal '<p>foo</p>', Hiccdown::to_html([:p, 'foo'])
   end
 
   def test_convert_tag_with_content_and_attrs
-    assert_equal Hiccdown::to_html([:p, {class: 'foo'}, 'bar']), '<p class="foo">bar</p>'
+    assert_equal '<p class="foo">bar</p>', Hiccdown::to_html([:p, {class: 'foo'}, 'bar'])
   end
 
   def test_convert_tag_with_children
     assert_equal(
       Hiccdown::to_html(
-        [:p, {class: 'foo'}, [:span, {class: 'bar'}, 'baz'], [:a, {href: '#foo'}, 'link']]
+        '<p class="foo"><span class="bar">baz</span><a href="#foo">link</a></p>'
       ),
-      '<p class="foo"><span class="bar">baz</span><a href="#foo">link</a></p>'
+      [:p, {class: 'foo'}, [:span, {class: 'bar'}, 'baz'], [:a, {href: '#foo'}, 'link']]
     )
   end
 
