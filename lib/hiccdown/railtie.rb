c7412ed827407a3d75b8c35c962995d616faf425
Dennis Hackethal <dh@dennishackethal.com>
2024-07-23 19:32:04 -0600

    Pass correct view_context to helper
    Previously, the controller-bound view_context, rather than the view-bound view_context, was sent to the helper. This resulted in a bug because helper and rendered view didn’t share state (breaking, eg, for content_for).

--- lib/hiccdown/railtie.rb
+++ lib/hiccdown/railtie.rb
@@ -7,6 +7,25 @@ module Hiccdown
     end
   end
 
+  class Renderable
+    def initialize(helper_module, action_name)
+      @helper_module = helper_module
+      @action_name = action_name
+    end
+
+    # This is the view-bound view_context. Needed for `content_for` to work
+    # properly in helper, see https://stackoverflow.com/a/78783866/1371131
+    def render_in(view_context)
+      content = @helper_module.instance_method(@action_name).bind_call(view_context)
+
+      Hiccdown::to_html(content)
+    end
+
+    def format
+      :html
+    end
+  end
+
   module CustomViewRendering
     extend ActiveSupport::Concern
 
@@ -45,8 +64,10 @@ module Hiccdown
       helper_module = helper_name.constantize
 
       if helper_module.instance_methods(false).include?(action_name.to_sym)
-        content = helper_module.instance_method(action_name).bind(view_context).call
-        original_render({ html: Hiccdown::to_html(content).html_safe, layout: !request.format.turbo_stream? }.merge(options))
+        original_render(
+          Hiccdown::Renderable.new(helper_module, action_name),
+          { layout: !request.format.turbo_stream? }.merge(options)
+        )
       else
         original_render({ action: action_name }.merge(options))
       end
